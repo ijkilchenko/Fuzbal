333cab5167b3ac0439888cd8fa1f352b626c2413
Alex Ilchenko <ijkilchenko@gmail.com>
2015-12-25 02:43:34 -0500

    Init commit (for GitHub)

---
+++ background.js
@@ -0,0 +1,40 @@
+/* author: ijkilchenko@gmail.com
+   MIT license */
+
+var vectors;
+$.getJSON("./vectors_dict.json", function(json) {
+	vectors = json;
+	console.log('word2vec dictionary loaded!');
+});
+
+chrome.runtime.onConnect.addListener(function(port) {
+	console.assert(port.name == "vectorsLookup");
+	port.onMessage.addListener(function(msg) {
+		function subset(keys, dict) {
+			dictSubset = {};
+			for (var i = 0; i < keys.length; i++) {
+				if (keys[i] in dict) {
+					dictSubset[keys[i]] = dict[keys[i]];
+				}
+			}
+			return dictSubset;
+		}
+		var localWords2Vects = {};
+		if (msg.words && msg.words.length > 0) {
+			localWords2Vects = subset(msg.words, vectors);
+		}
+		port.postMessage({localWords2Vects: localWords2Vects});
+	});
+});
+
+var stopWords;
+$.getJSON("./stopWords.json", function(json) {
+	stopWords = json;
+});
+
+chrome.runtime.onConnect.addListener(function(port) {
+	console.assert(port.name == "stopWordLookup");
+	port.onMessage.addListener(function(msg) {
+		port.postMessage({stopWords: stopWords});
+	});
+});
