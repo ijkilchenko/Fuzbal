6d8be88be9d807107a375b043c020ba7625430c9
hschroedl <hansj.schroedl@gmail.com>
2016-03-04 17:20:17 +0100

    Fix config, add dummy test

--- tests/something.spec.js
+++ tests/something.spec.js
@@ -1,3 +1,13 @@
 /**
  * Created by hschroedl on 04.03.16.
  */
+describe('something', function () {
+    'use strict';
+    describe('load', function () {
+
+        it('s hould do somethingt', function () {
+            expect(1).to.equal(1);
+        })
+    });
+
+});
